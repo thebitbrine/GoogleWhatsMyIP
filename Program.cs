59ce17bfccb20dfb506c1666787abcddaead746c
TheBitBrine <blacknomex08@gmail.com>
2019-04-29 17:19:20 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace GoogleWhatsMyIP
+{
+    class Program
+    {
+        static void Main()
+        {
+            var Response = new System.Net.WebClient() {Encoding = System.Text.Encoding.UTF8}.DownloadString("https://www.google.am/search?q=whats+my+ip&hl=en");
+            var m = new System.Text.RegularExpressions.Regex("((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\\d])").Match(Response);
+            if (m.Success)
+                Console.WriteLine(m.Groups[1].ToString());
+        }
+    }
+}
